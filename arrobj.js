39faa6c96aef7729e226e31e35c697d60a27af9c
gtush96 <35533179+gtush96@users.noreply.github.com>
2018-05-02 11:35:19 +0530

    Add files via upload

---
+++ arrobj.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+var stds=[{cgpa:5,e_count:3,f_count:1},
+          {cgpa:7,e_count:0,f_count:2},
+		  {cgpa:8,e_count:0,f_count:0}];
+		  
+
+	for(var key in stds){
+		//console.log(stds[key].cgpa);
+        	console.log("CGPA::"+stds[key].cgpa
+				+"  e_count::"+stds[key].e_count
+				+"  f_count::"+stds[key].f_count);
+        }
+console.log("CGPA::"+stds[0].cgpa
+				+"  e_count::"+stds[0].e_count
+				+"  f_count::"+stds[0].f_count);
